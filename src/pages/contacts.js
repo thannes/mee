60aeaf752a28853ab85764b40a6c4ee76b514d83
Tim Hannes <tim@nutrafol.com>
2020-10-13 14:52:28 -0400

    trying default form

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,24 +1,24 @@
 import React from "react"
 // import { Link } from "gatsby"
 import { Flex, Box } from "reflexbox"
-import styled from "styled-components"
+// import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Form = styled.form`
-  label {
-    span {
-      display: block;
-      margin-bottom: 5px;
-    }
-    input, textarea {
-      appearance: none;
-      width: 100%;
-      border: 1px solid #ccc;
-    }
-  }
-`
+// const Form = styled.form`
+//   label {
+//     span {
+//       display: block;
+//       margin-bottom: 5px;
+//     }
+//     input, textarea {
+//       appearance: none;
+//       width: 100%;
+//       border: 1px solid #ccc;
+//     }
+//   }
+// `
 
 const ContactsPage = () => (
   <Layout>
@@ -31,7 +31,7 @@ const ContactsPage = () => (
     </Flex>
     <Flex flexWrap="wrap" mx={-4}>
       <Box width={[1, null, 1/2]} px={4}>
-        <Form name="contact" method="POST" data-netlify="true">
+        {/* <Form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
           <p>
             <label><span>Name</span><input type="text" name="name" /></label>   
           </p>
@@ -45,7 +45,22 @@ const ContactsPage = () => (
           <p>
             <button type="submit" className="btn primary">Submit</button>
           </p>
-        </Form>
+        </Form> */}
+
+        <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+          <p>
+            <label>Email: <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <div data-netlify-recaptcha="true"></div>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+
+
       </Box>
       <Box width={[1, null, 1/2]} px={4}>
         <p>{`Matthias Erne Enterprises`}</p>
